0bd41dca17a0a0b1b706b14ab2447d0f45dd0a09
ancadenav <47695258+ancadenav@users.noreply.github.com>
2021-11-24 22:23:04 -0500

    Create trigger2C.sql

---
+++ trigger2C.sql
@@ -0,0 +1,254 @@
+CREATE OR REPLACE TRIGGER controlSalarioComisionEmpleadosUpdate
+FOR UPDATE ON empleado
+COMPOUND TRIGGER
+    salarioEmpleado empleado.salario%TYPE;
+    salarioJefe empleado.salario%TYPE;
+    jefeViejo empleado.jefe%TYPE;
+    codigoViejo empleado.codigo%TYPE;
+    salarioNuevo empleado.salario%TYPE;
+    salarioViejo empleado.salario%TYPE;
+    comis comision.valor%TYPE;
+    sumajef NUMBER(8);
+    sumaemp NUMBER(8);
+    sumaux NUMBER(8);
+
+    BEFORE EACH ROW IS
+    BEGIN
+        jefeViejo := :OLD.jefe;
+        codigoViejo := :OLD.codigo;
+        salarioNuevo := :NEW.salario;
+        salarioViejo := :OLD.salario;
+    END BEFORE EACH ROW;
+
+    AFTER STATEMENT IS
+    BEGIN
+        IF jefeViejo IS NULL THEN
+            FOR emp IN (SELECT salario, codigo FROM empleado WHERE jefe = codigoViejo)
+            LOOP
+                sumaux := 0;
+                FOR com IN (SELECT valor FROM comision WHERE codemp = emp.codigo)
+                
+                LOOP
+                    sumaux := sumaux + com.valor;
+                END LOOP;
+                sumaemp := sumaux + emp.salario;
+              
+                sumaux := 0;
+                FOR com IN (SELECT valor FROM comision WHERE codemp = codigoViejo)
+                
+                LOOP
+                    sumaux := sumaux + com.valor;
+                END LOOP;
+                sumajef := sumaux + salarioNuevo;
+                IF sumaemp > sumajef THEN
+                    UPDATE empleado SET salario = salarioViejo WHERE codigo = codigoViejo;
+                    RAISE_APPLICATION_ERROR(-20506,'La suma del salario y la comision del empleado es mayor a la de su jefe.');
+                    EXIT;
+                END IF;
+            END LOOP;
+        ELSE
+            sumaux := 0;
+            FOR com IN (SELECT valor FROM comision WHERE codemp = codigoViejo)
+            
+            LOOP
+                sumaux := sumaux + com.valor;
+            END LOOP;
+            sumaemp := sumaux + salarioNuevo;
+            
+            sumaux := 0;
+            SELECT salario into sumaux
+            FROM empleado WHERE codigo = jefeViejo;
+            FOR com IN (SELECT valor FROM comision WHERE codemp = jefeViejo)
+            
+            LOOP
+                sumaux := sumaux + com.valor;
+            END LOOP;
+            sumajef := sumaux;
+            IF sumajef < sumaemp THEN
+                UPDATE empleado SET salario = salarioViejo WHERE codigo = codigoViejo;
+                RAISE_APPLICATION_ERROR(-20505,'La suma del salario y la comision del empleado es mayor a la de su jefe.');
+            END IF;
+            FOR emp IN (SELECT salario, codigo FROM empleado WHERE jefe = codigoViejo)
+            LOOP    
+                sumaux := 0;
+                FOR com IN (SELECT valor FROM comision WHERE codemp = emp.codigo)
+                
+                LOOP
+                    sumaux := sumaux + com.valor;
+                END LOOP;
+                sumaemp := sumaux + emp.salario;
+                sumaux := 0;
+                FOR com IN (SELECT valor FROM comision WHERE codemp = codigoViejo)
+                
+                LOOP
+                    sumaux := sumaux + com.valor;
+                END LOOP;
+                sumajef := sumaux + salarioNuevo;
+                IF sumaemp > sumajef THEN
+                    UPDATE empleado SET salario = salarioViejo WHERE codigo = codigoViejo;
+                    RAISE_APPLICATION_ERROR(-20506,'La suma del salario y la comision del empleado es mayor a la de su jefe.');
+                    EXIT;
+                END IF;
+            END LOOP;
+        END IF;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            DBMS_OUTPUT.PUT_LINE('');
+    END AFTER STATEMENT;
+END controlSalarioComisionEmpleadosUpdate;
+
+
+CREATE OR REPLACE TRIGGER controlComisionUpdate
+FOR UPDATE ON comision
+COMPOUND TRIGGER
+    valorNuevo comision.valor%TYPE;
+    valorViejo comision.valor%TYPE;
+    codigoEmpleado comision.codemp%TYPE;
+    sumajef NUMBER(8);
+    sumaemp NUMBER(8);
+    sumaux NUMBER(8);
+    codaux NUMBER(8);
+
+    BEFORE EACH ROW IS
+    BEGIN
+        valorNuevo := :NEW.valor;
+        valorViejo := :OLD.valor;
+        codigoEmpleado := :OLD.codemp;
+    END BEFORE EACH ROW;
+
+    AFTER STATEMENT IS
+    BEGIN
+            
+        SELECT jefe INTO codaux
+        FROM empleado WHERE codigo = codigoEmpleado;
+        IF codaux IS NULL THEN
+            FOR emp IN (SELECT salario, codigo FROM empleado WHERE jefe = codigoEmpleado)
+            LOOP
+                SELECT salario INTO sumaux
+                FROM empleado WHERE codigo = emp.codigo;
+                FOR com IN (SELECT valor FROM comision WHERE codemp = emp.codigo)
+                LOOP
+                    sumaux := sumaux + com.valor;
+                END LOOP;
+                sumaemp := sumaux;
+                sumaux := 0;
+                SELECT salario INTO sumaux
+                FROM empleado WHERE codigo = codigoEmpleado;
+                FOR com IN (SELECT valor FROM comision WHERE codemp = codigoEmpleado)
+                
+                LOOP
+                    sumaux := sumaux + com.valor;
+                END LOOP;
+                sumajef := sumaux;
+                IF sumaemp > sumajef THEN
+                    UPDATE empleado SET salario = valorViejo WHERE codigo = codigoEmpleado;
+                    RAISE_APPLICATION_ERROR(-20506,'La suma del salario y la comision del empleado es mayor a la de su jefe.');
+                    EXIT;
+                END IF;
+            END LOOP;
+        ELSE
+            sumaux := 0;
+            SELECT salario into sumaux
+            FROM empleado WHERE codigo = codigoEmpleado;
+            FOR com IN (SELECT valor FROM comision WHERE codemp = codigoEmpleado)
+            
+            LOOP
+                sumaux := sumaux + com.valor;
+            END LOOP;
+            sumaemp := sumaux;
+            
+            sumaux := 0;
+            SELECT salario into sumaux
+            FROM empleado WHERE codigo = codaux;
+            FOR com IN (SELECT valor FROM comision WHERE codemp = codaux)
+            
+            LOOP
+                sumaux := sumaux + com.valor;
+            END LOOP;
+            sumajef := sumaux;
+            IF sumajef < sumaemp THEN
+                UPDATE empleado SET salario = valorViejo WHERE codigo = codigoEmpleado;
+                RAISE_APPLICATION_ERROR(-20505,'La suma del salario y la comision del empleado es mayor a la de su jefe.');
+            END IF;
+            FOR emp IN (SELECT salario, codigo FROM empleado WHERE jefe = codigoEmpleado)
+            LOOP    
+                sumaux := 0;
+                SELECT salario INTO sumaux
+                FROM empleado WHERE codigo = emp.codigo;
+                FOR com IN (SELECT valor FROM comision WHERE codemp = emp.codigo)
+                
+                LOOP
+                    sumaux := sumaux + com.valor;
+                END LOOP;
+                sumaemp := sumaux + emp.salario;
+                sumaux := 0;
+                SELECT salario INTO sumaux
+                FROM empleado WHERE codigo = codigoEmpleado;
+                FOR com IN (SELECT valor FROM comision WHERE codemp = codigoEmpleado)
+                
+                LOOP
+                    sumaux := sumaux + com.valor;
+                END LOOP;
+                sumajef := sumaux;
+                IF sumaemp > sumajef THEN
+                    UPDATE empleado SET salario = valorViejo WHERE codigo = codigoEmpleado;
+                    RAISE_APPLICATION_ERROR(-20506,'La suma del salario y la comision del empleado es mayor a la de su jefe.');
+                    EXIT;
+                END IF;
+            END LOOP;
+        END IF;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            DBMS_OUTPUT.PUT_LINE('');
+    END AFTER STATEMENT;
+END controlComisionUpdate;
+
+
+
+CREATE OR REPLACE TRIGGER controlComisionInsert
+FOR INSERT ON comision
+COMPOUND TRIGGER
+    codigoEmpleado comision.codemp%TYPE;
+    sumajef NUMBER(8);
+    sumaemp NUMBER(8);
+    sumaux NUMBER(8);
+    codaux NUMBER(8);
+
+    BEFORE EACH ROW IS
+    BEGIN
+        codigoEmpleado := :NEW.codemp;
+    END BEFORE EACH ROW;
+
+    AFTER STATEMENT IS
+    BEGIN
+            
+        SELECT jefe INTO codaux
+        FROM empleado WHERE codigo = codigoEmpleado;
+        SELECT salario INTO sumajef
+        FROM empleado WHERE codigo = codaux;
+        FOR com IN (SELECT valor FROM comision WHERE codemp = codaux)
+            
+        LOOP
+            sumaux := sumaux + com.valor;
+        END LOOP;
+        sumajef := sumaux;
+        
+        sumaux := 0;
+        SELECT salario into sumaux
+        FROM empleado WHERE codigo = codigoEmpleado;
+        FOR com IN (SELECT valor FROM comision WHERE codemp = codigoEmpleado)
+        
+        LOOP
+            sumaux := sumaux + com.valor;
+        END LOOP;
+        sumaemp := sumaux;
+        IF sumajef < sumaemp THEN
+            DELETE FROM empleado WHERE codigo = codigoEmpleado;
+            RAISE_APPLICATION_ERROR(-20505,'La suma del salario y la comision del empleado es mayor a la de su jefe.');
+        END IF;
+        
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            DBMS_OUTPUT.PUT_LINE('');
+    END AFTER STATEMENT;
+END controlComisionInsert;
